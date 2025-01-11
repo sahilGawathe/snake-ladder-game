cb55b58b35ae249707f7a8d4097bf32d3c5975dc
sahil gawathe <sahilgawathe4956@gmail.com>
2025-01-11 10:28:19 +0530

    Added: Initilize Plyer1 Start Position to 0

--- src/snakeAndLadderGame/SnakeLadderGame.java
+++ src/snakeAndLadderGame/SnakeLadderGame.java
@@ -1,8 +1,10 @@
 package snakeAndLadderGame;
 
-
 public class SnakeLadderGame {
+	static int player1 = 0; // Static Global Variable Player 1 Starting Position and 0 Postion on board
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake Ladder Game!");
 	}
+	
+
 }
