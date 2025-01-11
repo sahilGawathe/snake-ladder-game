222229264f73b324c243adc00909cdd5b282a257
sahil gawathe <sahilgawathe4956@gmail.com>
2025-01-11 10:30:09 +0530

    Added: rollDice function to return random roll between 1 to 6.

--- src/snakeAndLadderGame/SnakeLadderGame.java
+++ src/snakeAndLadderGame/SnakeLadderGame.java
@@ -1,10 +1,16 @@
 package snakeAndLadderGame;
+import java.util.Random;
 
 public class SnakeLadderGame {
 	static int player1 = 0; // Static Global Variable Player 1 Starting Position and 0 Postion on board
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake Ladder Game!");
+
 	}
 	
-
+	private static int rollDice() {
+		Random random = new Random();
+		return random.nextInt(6)+1;
+				
+	}
 }
