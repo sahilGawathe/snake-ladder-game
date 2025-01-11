4f1c65e866bb3d5c675282bfef8f8bcab2fb10e9
sahil gawathe <sahilgawathe4956@gmail.com>
2025-01-11 10:31:46 +0530

    Added: Get Option and playGame Methods till UC3

--- src/snakeAndLadderGame/SnakeLadderGame.java
+++ src/snakeAndLadderGame/SnakeLadderGame.java
@@ -2,15 +2,45 @@ package snakeAndLadderGame;
 import java.util.Random;
 
 public class SnakeLadderGame {
-	static int player1 = 0; // Static Global Variable Player 1 Starting Position and 0 Postion on board
+	static int position = 0; // Static Global Variable Player 1 Starting Position and 0 Postion on board
+	static int winningPosition = 100; // Static Golbal Winning Postion 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake Ladder Game!");
-
+		playGame();
+		
+	}
+	private static void playGame() {
+		int diceRoll=0;
+		
+		while(position < winningPosition) {
+			int dieRoll = rollDice();
+			diceRoll++;
+			System.out.println("Rolled : "+dieRoll);
+			
+			switch(getOption()) {
+			case 0:
+				System.out.println("No Play: Player Stay at Position "+position);
+				break;
+			case 1:
+				position += dieRoll;
+				System.out.println("Ladder : Move Ahead to Positon "+position);
+				break;
+			case 2:
+				position -= dieRoll;
+				System.out.println("Snake : Move Back to Positon "+ position);
+			}
+			
+		}
+		System.out.println("Congratulations! You reached position 100 in " + diceRoll + " rolls.");
 	}
 	
 	private static int rollDice() {
 		Random random = new Random();
 		return random.nextInt(6)+1;
 				
+	}
+	private static int getOption() {
+		Random random = new Random();
+		return random.nextInt(3);
 	}
 }
